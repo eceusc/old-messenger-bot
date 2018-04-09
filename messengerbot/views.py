08e5699fecdd3bc68f4a6c5795f4ce4f7ddd9675
Alex Garcia <alexsebastian.garcia@gmail.com>
2018-04-09 01:11:40 -0700

    testing heroku auto deploy

--- messengerbot/views.py
+++ messengerbot/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse('Front page of ECEUSC Messenger Bot should go here!')
+    return HttpResponse('Front page of ECEUSC Messenger Bot should go here! Testing heroku...')
 
 def webhook(request):
     return HttpResponse('Lol lets do this')
