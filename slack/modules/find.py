b0686a4865e1108e0d523b41504d4db60da16085
Alex Garcia <alexsebastian.garcia@gmail.com>
2018-04-18 20:53:11 -0700

    trying new find feature

---
+++ slack/modules/find.py
@@ -0,0 +1,11 @@
+# find.py
+
+def process(words):
+    role = words[0].lower()
+    
+    if role == 'president':
+        return 'Alex Garcia'
+    else:
+        return 'whoops'
+    
+    return s
