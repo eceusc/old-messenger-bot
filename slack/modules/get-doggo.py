cde0c7a6b26977e421aefa729fe0c35ab68588e3
Alex Garcia <alexsebastian.garcia@gmail.com>
2018-04-20 13:19:33 -0700

    wow alex test your code before pushing - fix get-doggo

--- slack/modules/get-doggo.py
+++ slack/modules/get-doggo.py
@@ -1,6 +1,6 @@
 import requests
 def process(words):
-    l 'https://random.dog/woof.json'
+    l = 'https://random.dog/woof.json'
     r = requests.get(l).json()
     return {
             "response_type":"ephermal",
