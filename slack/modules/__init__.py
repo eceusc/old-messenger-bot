812dce819cef1a30241b06caa8a229b3a7adbf74
oesan1 <oesan@ucsd.edu>
2018-04-24 13:30:44 -0700

    added tomi.py

--- slack/modules/__init__.py
+++ slack/modules/__init__.py
@@ -3,4 +3,5 @@ __all__ = [
     'galex',
     'find',
     'get-doggo',
+    'tomi',
 ]
---
+++ slack/modules/tomi.py
@@ -0,0 +1,7 @@
+def process(words):
+    if words[0] == 'tomi':
+        return 'thats my bitch. i fucking love her'
+    else:
+        return 'hello bitches! this is my shit!'
+    
+
