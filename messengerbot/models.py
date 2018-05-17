9104793a31859be622f64a372e11bbb3d4c37a91
Alex Garcia <alexsebastian.garcia@gmail.com>
2018-05-17 11:39:35 -0700

    upstream changes, my own

--- messengerbot/models.py
+++ messengerbot/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    psid = models.CharField(max_length=30, unique=True)
+    ucsd_email = models.CharField(max_length=50)
+    
+    def __str__(self):
+        email = self.ucsd_email or '  '
+        return '{{ {} | {} | {}...@ucsd.edu}}'.format(self.id, self.psid, self.ucsd_email[:2])
