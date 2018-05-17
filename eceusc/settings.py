b1f1ddfa0c26fbcf2062f824ab6a1fb7cb1ba692
Alex Garcia <alexsebastian.garcia@gmail.com>
2018-05-17 12:15:04 -0700

    hopefully add db to heroku

--- eceusc/settings.py
+++ eceusc/settings.py
@@ -89,6 +89,10 @@ DATABASES = {
     }
 }
 
+if os.environ.get('DATABASE_URL') is not None:
+    import dj_database_url
+    print("using database url env var...")
+    DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
